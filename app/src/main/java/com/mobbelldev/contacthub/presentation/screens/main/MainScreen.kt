abb24cc1ac3315abc6ee482b364d778e0446c1f4
Zaenal Arif <zaenalariftech@gmail.com>
2025-08-26 00:15:43 +0700

    refactor: update null data message to indicate connection issue

--- app/src/main/java/com/mobbelldev/contacthub/presentation/screens/main/MainScreen.kt
+++ app/src/main/java/com/mobbelldev/contacthub/presentation/screens/main/MainScreen.kt
@@ -87,7 +87,7 @@ fun MainScreen(
                         .padding(paddingValues = paddingValues),
                     contentAlignment = Alignment.Center
                 ) {
-                    Text(text = stringResource(R.string.data_is_null))
+                    Text(text = stringResource(R.string.connection))
                 }
             }
 
